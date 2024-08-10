2ef0a1a5cb73c0047367648078d857181d368065
sidaswal7 <siddhantaswal7@gmail.com>
2024-08-10 15:48:10 +0530

    second commit

--- Intro/firstCode.js
+++ Intro/firstCode.js
@@ -1,1 +1,15 @@
 console.log("Hello Nodejs");
+
+let product = (a,b)=> a*b;
+console.log(product(3,4))
+
+
+let student = {
+    name: "Siddhant",
+    age: 21,
+    getInfo : function(){
+        console.log("My name is", this.name)
+    }
+}
+
+student.getInfo()
